03bf49b7837ae07aa20ce47f4d053c641df0144d
Eddy Collado <eddcollado@gmail.com>
2019-12-22 21:17:52 -0700

    updated sources for charCodesheet

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function writePassword() {
 
 
 
-// Generator functions
+// Generator functions - http://www.net-comber.com/charset.html
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
