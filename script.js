b1cf343d5d9f3565eef7d54eab3b4eb09ac4eba8
Eddy Collado <eddcollado@gmail.com>
2019-12-23 16:30:44 -0700

    Was able to apply text to password box, however, having trouble converting object into number

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 // Assignment Code/ DOM elements
 var generateBtn = document.querySelector("#generate");
+//Trying to see what a preset length would do
+var lengthE1 = 12;
 
 
 
 // Attempting to build a for loop that will generate through the functions
 //of the randomFunc object to generate a password
-function generatePassword() {
-  for (var i = 0; i < randomFunc.length; i++ ) {
- 
+
+// Generator functions - http://www.net-comber.com/charset.html  
+  function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
   }
-}
+  
+  function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+  
+  function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
+  
+  function getRandomSymbol() {
+    var symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  }
+
+  var randomFunc = [
+    {lower: getRandomLower()},
+    {upper: getRandomUpper()},
+    {number: getRandomNumber()},
+    {symbol: getRandomSymbol()
+  },{lower: getRandomLower()},
+  {upper: getRandomUpper()},
+  {number: getRandomNumber()},
+  {symbol: getRandomSymbol()
+},{lower: getRandomLower()},
+{upper: getRandomUpper()},
+{number: getRandomNumber()},
+{symbol: getRandomSymbol()
+}];
 
 
 // Write password to the #password input
@@ -19,49 +49,47 @@ function writePassword() {
   var lengthQ = prompt("Please indicate the length of desired password");
   
 
-  if (lengthQ === "") {
-  alert("please enter a numerical value");
-} else {
-  passwordText.value = password;
-}
+  if (lengthQ === "12") {
+    passwordText.textContent = password;
+    passwordText.innerHTML = randomFunc;
+    
+
+    generatePassword();
+    
+    
+  } else {
+    alert("please enter 12");
+  
+  }
+
+  function generatePassword() {
+    for (var i = 0; i < randomFunc.length; i++ ) {
+      
+      console.log(randomFunc[i]);
+    }
+  }
+
+
+  
+
 
 
 // Copy to clipboard 
 // copyBtn.setAttribute();
 // copyBtn.focus();
 
+
+
 }
 
 //Generate random characters
-  var randomFunc = [
-  {lower: getRandomLower()},
-  {upper: getRandomUpper()},
-  {number: getRandomNumber()},
-  {symbol: getRandomSymbol()
-}];
-
+  
 
 
 // Generator functions - http://www.net-comber.com/charset.html
 
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
 
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
 
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  var symbols = '!@#$%^&*(){}[]=<>/,.'
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-console.log(randomFunc);
 
 
 
